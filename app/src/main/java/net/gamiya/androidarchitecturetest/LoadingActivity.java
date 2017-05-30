ace4389c4686da4aaebdaaf45d4b62a1f721e7b5
System Administrator <root@VISHWAN-PC.local>
2017-05-30 09:28:00 +0530

    basic android project - complete

---
+++ app/src/main/java/net/gamiya/androidarchitecturetest/LoadingActivity.java
@@ -0,0 +1,18 @@
+package net.gamiya.androidarchitecturetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class LoadingActivity extends Activity {
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		  setContentView(R.layout.main);
+
+	}
+	
+}
