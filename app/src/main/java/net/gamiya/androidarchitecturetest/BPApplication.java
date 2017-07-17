0ae5bc07245e27c79d7c505f0681522f99055644
Kalan Nawarathne <kalan@Kalans-MacBook-Pro.local>
2017-07-17 20:11:07 +0530

    added dagger

---
+++ app/src/main/java/net/gamiya/androidarchitecturetest/BPApplication.java
@@ -0,0 +1,31 @@
+package net.gamiya.androidarchitecturetest;
+
+import android.app.Activity;
+import android.app.Application;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+/**
+ * Created by kalan on 7/15/17.
+ */
+
+public class BPApplication extends Application implements HasActivityInjector {
+
+    @Inject DispatchingAndroidInjector<Activity> dispatchingActivityInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DaggerYourApplicationComponent.create()
+                .inject(this);
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingActivityInjector;
+    }
+}
